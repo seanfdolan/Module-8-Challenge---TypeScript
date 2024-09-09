d387b887b673402096b8ce84305be57e5921dcba
Sean Dolan <sean.f.dolan@gmail.com>
2024-09-09 18:34:38 -0400

    Add files via upload

---
+++ src/Truck.ts
@@ -0,0 +1,80 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+// TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+class Truck {
+  name: string
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
+  // TODO: Declare properties of the Truck class
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+
+  constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[], towingCapacity: number) {
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    this.towingCapacity = towingCapacity;
+  }
+  // TODO: Create a constructor that accepts the properties of the Truck class
+    // TODO: The constructor should call the constructor of the parent class, Vehicle
+    // TODO: The constructor should initialize the properties of the Truck class
+    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+
+  // TODO: Implement the tow method from the AbleToTow interface
+  //
+  tow(vehicle: Truck | Motorbike | Car): void {
+    // TODO: Get the make an model of the vehicle if it exists
+    function tow(vehicle: Truck | Motorbike | Car): void {
+      if (vehicle instanceof Truck) {
+        console.log(`Towing ${vehicle.make} ${vehicle.model}`);
+      } else {
+        console.log(`Towing vehicle`);
+      }
+    }
+    // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+    if (vehicle.weight <= this.towingCapacity) {
+      console.log(`Towing ${vehicle.make} ${vehicle.model}`);
+    } else {
+      console.log(`Vehicle is too heavy to be towed`);
+    }
+    // TODO: If it is, log that the vehicle is being towed
+    // TODO: If it is not, log that the vehicle is too heavy to be towed
+  }
+printDetails(): void {
+  console.log('VIN: ${this.vin}');
+  console.log('Make: ${this.make}');
+  console.log('Model: ${this.model}');
+  console.log('Year: ${this.year}');
+  console.log('Weight: ${this.weight}');
+  console.log('Top Speed: ${this.topSpeed}');
+  console.log('Color: ${this.color}');
+  console.log('Towing Capacity: ${this.towingCapacity}');
+  console.log('Wheels: ${this.wheels.length}');
+  // TODO: Override the printDetails method from the Vehicle class
+    // TODO: The method should call the printDetails method of the parent class
+    // TODO: The method should log the details of the Truck
+    // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+}
+
+// Export the Truck class as the default export
+  /**
+   * Represents a Truck.
+   */
+  export: any default: any Truck: any;}
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,25 @@ import Wheel from "./classes/Wheel.js";
 import Cli from "./classes/Cli.js";
 
 // create an array of vehicles
-const vehicles = [];
+const vehicles = [
+  // new Truck("1FTFW1ET4BFB73524", "red", "Ford", "F-150", 2021, 5000, 120, [], 10000),
+  new Car("1FTFW1ET4BFB73524", "blue", "Toyota", "Camry", 2021, 3000, 130, [
+    new Wheel(17, "Michelin"),
+    new Wheel(17, "Michelin"),
+    new Wheel(17, "Michelin"),
+    new Wheel(17, "Michelin"),
+  ]),
+  new Motorbike("1FTFW1ET4BFB73524", "black", "Harley Davidson", "Sportster", 2021, 500, 125, [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")])
+];
+
+
 
 // TODO: uncomment once trucks are implemented
-// const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const truck1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin"), new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
+truck1.wheels = truck1Wheels;
+console.log(truck1);
+
 
 // will use default wheels
 const car1 = new Car(
@@ -24,15 +39,16 @@ const car1 = new Car(
 );
 
 // TODO: uncomment once motorbikes are implemented
-// const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
-// const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
+const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+console.log(motorbike1);
 
 // push vehicles to array
 // TODO: uncomment once trucks are implemented
-// vehicles.push(truck1);
+vehicles.push(truck1);
 vehicles.push(car1);
 // TODO: uncomment once motorbikes are implemented
-// vehicles.push(motorbike1);
+vehicles.push(motorbike1);
 
 // create a new instance of the Cli class
 const cli = new Cli(vehicles);
